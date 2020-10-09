40f788bc7feab4ea61f4a6e8e3ab9557ec02eebc
Wietse Wind <mail@wietse.com>
2020-10-09 12:21:58 +0200

    Simpler JS sample

--- samples/dev.js
+++ samples/dev.js
@@ -1,26 +1,10 @@
-const {TxData} = require('../dist/src/index')
-// const {TxData} = require('xrpl-txdata')
+const {TxData} = require('xrpl-txdata')
 
-const main = async () => {
-  try {
-    const txd = new TxData([
-      'wss://xrpl.ws',
-      'wss://s2.ripple.com'
-    ], {
-      EndpointTimeoutMs: 1500,
-      OverallTimeoutMs: 9000
-    })
+const someTxHash = 'B34EFCDA6A9D6B19670E19ECD3CFD638177EED92B863756DD96CFD197B940515'
 
-    const tx = await txd.getOne('B34EFCDA6A9D6B19670E19ECD3CFD638177EED92B863756DD96CFD197B940515') // Real
-    console.log(tx.host + ' replied', tx.result)
+const tx = (new TxData()).getOne(someTxHash).thenN(tx => {
+  console.log(tx.result)
 
-    const myBalanceChangesInXrp = tx.balanceChanges[tx.result.Account].filter(t => t.currency === 'XRP')
-    console.log({
-        SendingAccountXrpBalanceChange: Number(myBalanceChangesInXrp[0].value)
-    })
-  } catch (e) {
-    console.log('Error', e)
-  }
-}
-
-main()
+  const xrpBalanceChange = tx.balanceChanges[tx.result.Account].filter(t => t.currency === 'XRP')
+  console.log(Number(xrpBalanceChange[0].value))  
+})
